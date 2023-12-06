233c27ad37b500ba5b1b7960c9cf8c8c72a4d1a6
iconde <iconde@Ubuntu20.04.myguest.virtualbox.org>
2023-12-06 18:25:38 +0100

    Added final project implementation and test

--- base_external/package/aesdchar/aesdchar.mk
+++ base_external/package/aesdchar/aesdchar.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-AESDCHAR_VERSION = 'b16fe6717c07fb2a9713083f5d07154e398f698a'
+AESDCHAR_VERSION = 'ff9a5708350e4281aa18f386f5c87e2917124569'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -18,6 +18,7 @@ AESDCHAR_MODULE_SUBDIRS = aesd-char-driver/
 define AESDCHAR_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/final_project_test.sh $(TARGET_DIR)/usr/bin/
 endef
 
 $(eval $(kernel-module))
